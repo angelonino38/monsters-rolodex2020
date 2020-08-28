fd5c8896aa2699306b2b8f8d661c470081e7f73d
Angelo Nino <angelo@yahoo.com>
2020-08-29 01:58:28 +0800

    Updates

---
+++ precache-manifest.d5ce8967357b0ebdbbeb49f15568c560.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13aaaa1bedf012cf6cd6e60f300ce993",
+    "url": "/monsters-rolodex26/index.html"
+  },
+  {
+    "revision": "1caa9b042c39f5ffb9d0",
+    "url": "/monsters-rolodex26/static/css/main.3d249f60.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/monsters-rolodex26/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex26/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1caa9b042c39f5ffb9d0",
+    "url": "/monsters-rolodex26/static/js/main.3ae09f5f.chunk.js"
+  },
+  {
+    "revision": "f407ab10785fde5de0f1",
+    "url": "/monsters-rolodex26/static/js/runtime-main.dc92d72d.js"
+  }
+]);
